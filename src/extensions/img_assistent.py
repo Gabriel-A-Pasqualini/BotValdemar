ed42044a6c7970a3873e04add325d6133780f515
Gabriel Pasqualini <gabrielpasqualini2@gmail.com>
2023-07-17 23:34:26 -0300

    Atualização no comando slash

--- src/extensions/img_assistent.py
+++ src/extensions/img_assistent.py
@@ -5,8 +5,8 @@ import discord
 import interactions
 import openai
 from utils.img import create
-
 from utils.translator import translateToEn
+
 
 with open('../token.txt') as tk:
     token = tk.readlines()[0]
@@ -17,36 +17,48 @@ class Image(interactions.Extension):
     def __init__(self, client: interactions.Client) -> None:
         self.client: interactions.Client = client
 
-    
-    @interactions.slash_command("gerar-imagem", description="Gero uma imagem baseada em um texto!")
-    async def img(self, ctx: interactions.SlashContext):
+
+  
+    @interactions.slash_command("gerar-imagem",
+                                description="Gero uma imagem baseada em um texto!",
+                                options=[
+                                        interactions.Option(
+                                                                name="text",
+                                                                description="What you want to say",
+                                                                type=interactions.OptionType.STRING,
+                                                                required=True,
+                                                            ),
+                                        ]
+                                )                                           
+    async def img(self, ctx: interactions.SlashContext,txt: str):                     
+        
         time = 70
 
-        print(ctx.message)
+        print(txt)
 
-        text = 'gato correndo na agua'
+        text = 'faça um desenho colorido que represente o sentiemento de qualia do ser humano diante do vasto universo'
 
         texToTrans = translateToEn(text)
         genImg = threading.Thread(target=create, args=(texToTrans,))
-        await ctx.send(f'Await {time} sec!')
 
-        sleep(3)
+        message = await ctx.send(f'Await {time} sec!')
 
         for i in range(time):    
             if i < 1:
                 genImg.start()
-            await ctx.edit(content=f'{i} / {time-1}!') 
+            await message.edit(content=f'{i} / {time-1}!') 
             sleep(1)             
             i+1        
-            await ctx.send(f'I will send {texToTrans}, now!')
+        
+        await ctx.send(f'I will send {texToTrans}, now!')
 
-            path = f'./img\{texToTrans}.png'
+        path = f'./img\{texToTrans}.png'
                 
-            while not check_file:
-                check_file = os.path.isfile(path)
-                await ctx.send(f'You image is not ready. I will try again...')
+            #while not check_file:
+            #    check_file = os.path.isfile(path)
+            #    await ctx.send(f'You image is not ready. I will try again...')
 
-            await ctx.send(file=discord.File(path))
+        await ctx.send(file=path)
 
 def setup(client):
     Image(client)
--- src/old.py
+++ src/old.py
@@ -9,19 +9,9 @@ from utils.translator import translateToEn
 with open('../token.txt') as tk:
     token = tk.readlines()[0]
 
-
 intents = discord.Intents.all()
 intents.message_content = True
 client = discord.Client(intents=intents)
-#client = discord.Client(command_prefix='/', intents=intents)
-bot = commands.Bot(command_prefix='/',intents=intents)
-
-
-
-@bot.slash_command(name="first_slash", guild_ids=[337790268818587659]) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
-async def first_slash(ctx): 
-    await ctx.respond("You executed the slash command!")
-
 
 @client.event
 async def on_ready():
@@ -33,13 +23,12 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-
-    if message.content.startswith('hello'):        
+    if message.content.startswith('$hello'):        
         message = await message.channel.send(f'Hello @{message.author.nick}! You are in {message.guild.name}')
         print(message)
 
-    if message.content.startswith('img'):
-        time = 70
+    if message.content.startswith('$img'):
+        time = 51
         text = message.content.split('img ')[1]
 
         texToTrans = translateToEn(text)
@@ -56,13 +45,12 @@ async def on_message(message):
             i+1        
         await message.channel.send(f'I will send {texToTrans}, now!')
 
-        path = f'./img\{texToTrans}.png'
+        path = f'./img/{texToTrans}.png'
         
-        while not check_file:
-            check_file = os.path.isfile(path)
-            await message.channel.send(f'You image is not ready. I will try again...')
+        #while not check_file:
+        #    check_file = os.path.isfile(path)
+        #    await message.channel.send(f'You image is not ready. I will try again...')
 
         await message.channel.send(file=discord.File(path))
         
 client.run(token)
-bot.run(token)
